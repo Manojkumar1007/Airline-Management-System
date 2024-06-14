8e066b1c2a71468050500056fc50eaa0601d3c67
Manoj Kumar Atmakuri <manojkumaratmakuri@gmail.com>
2024-06-14 15:40:26 +0530

    made some changes in login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import LoginPage from "./pages/LoginPage/Loginpage";
 import Signup_page from "./pages/signup_page/signup_page";
+import HomePage from './pages/HomePage/HomePage';
 
 function App() {
   
@@ -12,6 +13,7 @@ function App() {
             <Route path='/login' Component={LoginPage} />
             <Route path='/signup' Component={Signup_page} />
             <Route path='/' Component={LoginPage} />
+            <Route path='/home' Component={HomePage} />
 
           </Routes>
         </div>
--- src/pages/LoginPage/Loginpage.jsx
+++ src/pages/LoginPage/Loginpage.jsx
@@ -1,22 +1,50 @@
+import {useState} from "react";
+import axios from 'axios';
+import {useHistory} from 'react-router-dom';
 import { Link } from "react-router-dom";
-import "./LoginPage.css"
+import "./LoginPage.css";
 
-function LoginPage(){
-    return(
-        <div className="container">
-        <h2>Login</h2>
-        <form id="loginForm">
-            <label htmlFor="email">Email:</label>
-            <input type="email" id="email" name="email" required />
-            
-            <label htmlFor="password">Password:</label>
-            <input type="password" id="password" name="password" required />
-            
-            <button className="LoginButton" type="submit">Login</button>
-        </form>
-        <p>Don&apos;t have an account? <Link to="/signup">Sign up here </Link></p>
-      </div>
-    );
+function LoginPage() {
+
+    const [formData, setFormData] = useState({
+        email: '',
+        password: '',
+    });
+
+    const { email , password } = formData;
+    const history = useHistory();
+
+    const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
+
+    const onSubmit = async e => {
+        e.preventDefault();
+        try {
+            await axios.post('http://localhost:5000/user/signin', formData);
+            history.push('/home');
+        } catch (error) {
+            console.error(error.response.data);
+        }
+    }
+
+  return (
+    <div className="container">
+      <h2>Login</h2>
+      <form id="loginForm" onSubmit={onSubmit}>
+        <label htmlFor="email">Email:</label>
+        <input type="email" id="email" name="email" value={email} onChange={onChange} required />
+
+        <label htmlFor="password">Password:</label>
+        <input type="password" id="password" name="password" value={password} onChange={onChange} required />
+
+        <button className="LoginButton" type="submit">
+          Login
+        </button>
+      </form>
+      <p>
+        Don&apos;t have an account? <Link to="/signup">Sign up here </Link>
+      </p>
+    </div>
+  );
 }
 
-export default LoginPage 
+export default LoginPage;
