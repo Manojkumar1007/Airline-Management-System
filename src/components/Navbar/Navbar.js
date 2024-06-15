0fd87eb8d3be0516d065cafb43accf4164e4a76a
sreehithabasireddy <sreehithabasireddy9920@gmail.com>
2024-06-15 17:02:48 +0530

    added components and helper

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Navbar.css";
+import {NavLink} from "react-router-dom";
+const Navbar = () => {
+    return(
+        <div className="navbar">
+            <div className="airlines">
+                <h3>@ApnaFlights</h3>
+            </div>
+            <div className="paths">
+                <ul>
+                    <NavLink to = '/'><li>Home</li></NavLink>
+                    <NavLink to = './login'><li>Log-in</li></NavLink>
+                </ul>
+                
+            </div>
+        </div>
+    )
+}
+export default Navbar;
