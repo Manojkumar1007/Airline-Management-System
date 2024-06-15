c5a62c16b06927f7539e9ebb1b6ded7ce349c828
Manoj Kumar Atmakuri <manojkumaratmakuri@gmail.com>
2024-06-16 00:13:55 +0530

    modifications in searchflights.jsx

--- src/pages/Searchflights/searchflights.jsx
+++ src/pages/Searchflights/searchflights.jsx
@@ -5,22 +5,27 @@ import { Info } from "../../Helper/helper";
 
 const Searchflights = () => {
   const { allInf, setAllInf } = useContext(Info);
-  const { from, to, travellingdate } = allInf;
+  const { startingCity, destinationCity, traveldate } = allInf;
 
   const changeHandler = e => {
     setAllInf({ ...allInf, [e.target.name]: e.target.value });
   }
 
   const navigate = useNavigate();
-  const [search, setSearch] = useState(false);
+  //const [search, setSearch] = useState(false);
 
-  if (search) {
-    if (from && to && travellingdate) {
-      return navigate('/flightdisplay');
-    } else {
-      alert("Please fill in all the details.");
-    }
-  }
+  // if (search) {
+  //   if (from && to && travellingdate) {
+  //     return navigate('/flightdisplay');
+  //   } else {
+  //     alert("Please fill in all the details.");
+  //   }
+  // }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    navigate('/flightdisplay', {state: {startingCity,destinationCity,traveldate}});
+  };
 
   return (
     <div>
@@ -28,10 +33,10 @@ const Searchflights = () => {
       <div className='box'>
         <h5 className='one-way-title'>One-way</h5>
         <center>
-          <form>
+          <form onSubmit={handleSubmit}>
             <div className='form-grp'>
               <label>From:🛫</label><br />
-              <select name='from' value={from} onChange={changeHandler}>
+              <select name='from' value={startingCity} onChange={changeHandler} required>
                 <option value='' disabled>Select departure city</option>
                 <option value='Bangalore'>Bangalore</option>
                 <option value='Mumbai'>Mumbai</option>
@@ -42,7 +47,7 @@ const Searchflights = () => {
             </div>
             <div className='to-grp'>
               <label>To:🛬</label><br />
-              <select name='to' value={to} onChange={changeHandler}>
+              <select name='to' value={destinationCity} onChange={changeHandler} required>
                 <option value='' disabled>Select destination city</option>
                 <option value='Guwahati'>Guwahati</option>
                 <option value='Kolkata'>Kolkata</option>
@@ -53,10 +58,11 @@ const Searchflights = () => {
             </div>
             <div className='date-grp'>
               <label>Departure Date</label><br />
-              <input type='date' name='travellingdate' value={travellingdate} onChange={changeHandler} />
+              <input type='date' name='travellingdate' value={traveldate} onChange={changeHandler} required/>
             </div>
             <div>
-              <button type='button' className='search-btn' onClick={() => setSearch(true)}>Search Flights🔍</button><br />
+              {/* <button type='button' className='search-btn' onClick={() => setSearch(true)}>Search Flights🔍</button><br /> */}
+              <button type='submit' className='search-btn'>Search Flights🔍</button>
             </div>
           </form>
         </center>
--- src/pages/flightsdisplay/flightdisplay.jsx
+++ src/pages/flightsdisplay/flightdisplay.jsx
@@ -1,14 +1,15 @@
-import React, { useContext } from "react";
+import React, { useState, useEffect } from "react";
 import Flightcard from "./flightcard";
 import details from "./flightdetails";
 import { Info } from "../../Helper/helper";
+import { useLocation} from "react-router-dom";
+
 const Createflight =() => {
   return (
     <Flightcard
       key={details.id}
       flightname={details.flightname}
       flightnum={details.flightnum}
-      img={details.img}
       starttime={details.starttime}
       startingcity={details.from}
       duration={details.duration}
@@ -20,9 +21,50 @@ const Createflight =() => {
   );
 }
 function Flightsdisplay() {
-  const {allInf,setAllInf} = useContext(Info);
+  //const {allInf,setAllInf} = useContext(Info);
+  const location = useLocation();
+  const {startingCity, destinationCity, travelDate} = location.state;
+  const [flights, setFlights] = useState([]);
+
+  useEffect(() => {
+    const fetchFlights = async () => {
+      const response = await fetch('http://localhost:5000/admin/flights/search',{
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({startingCity, destinationCity, travelDate}),
+      });
+      const data = await response.json();
+      setFlights(data);
+    };
+
+    fetchFlights();
+  }, [startingCity, destinationCity, travelDate]);
+
   return (
-    details.map(Flightcard)
+    //details.map(Flightcard)
+    <div>
+      {flights.length > 0 ? (
+        flights.map((flight) => (
+          <Flightcard 
+            key={flight._id}
+            id={flight._id}
+            flightname={flight.companyName}
+            flightnum={flight.flightNumber}
+            starttime={new Date(flight.startTime).toLocaleTimeString()}
+            duration={`${Math.floor((new Date(flight.endTime)-new Date(flight.startTime))/60000)} min`}
+            layovers={flight.layovers}
+            endtime={new Date(flight.endTime).toLocaleTimeString()}
+            price={`Rs. ${flight.pricePerSeat}`}
+            from={flight.startingCity}
+            to={flight.destinationCity}
+          />
+        ))
+      ):(
+        <p>No flights found 😔</p>
+      )}
+    </div>
   );
 }
 export default Flightsdisplay;
