b18be16f77be6ebfb8b69f1e3ddd929aa4913a67
Manoj Kumar Atmakuri <manojkumaratmakuri@gmail.com>
2024-06-18 21:39:11 +0530

    Added profile details to localStorage

--- src/Helper/ProfileContext.jsx
+++ src/Helper/ProfileContext.jsx
@@ -1,13 +1,20 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useEffect, useState } from 'react';
 
 export const ProfileContext = createContext();
 
 export const ProfileProvider = ({ children }) => {
-  const [profile, setProfile] = useState({
-    fullName: 'user',
-    email: 'abcdef@gmail.com',
-    mobileNumber: '+91 123456789',
+  const [profile, setProfile] = useState(() => {
+    const savedProfile = localStorage.getItem('profile');
+    return savedProfile ? JSON.parse(savedProfile) : {
+      fullName: 'user',
+      email: 'abcdef@gmail.com',
+      mobileNumber: '+91 123456789',
+    }
   });
+
+  useEffect(() => {
+    localStorage.setItem('profile', JSON.stringify(profile));
+  }, [profile]);
 
   return (
     <ProfileContext.Provider value={{ profile, setProfile }}>
